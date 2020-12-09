9f6ddf38ec897ad3b185000cbb1c1903426185a3
Chris Wilson <chris.wilson.7374@gmail.com>
2020-12-09 18:01:01 +0000

    Use cloudformation deploy for cloudformation example

--- aws-cloudformation/apply.sh
+++ aws-cloudformation/apply.sh
@@ -2,13 +2,4 @@
 
 set -euo pipefail
 
-set +e
-aws --profile personal --region eu-west-1 cloudformation describe-stacks --stack-name cloudformation-vpc
-STACK_EXISTS=$( echo $? )
-set -e
-
-if [ ${STACK_EXISTS} == 0 ]; then
-  aws --profile personal --region eu-west-1 cloudformation update-stack --stack-name cloudformation-vpc --template-body file://vpc.yaml
-else
-  aws --profile personal --region eu-west-1 cloudformation create-stack --stack-name cloudformation-vpc --template-body file://vpc.yaml
-fi
+aws --profile personal --region eu-west-1 cloudformation deploy --stack-name cloudformation-vpc --template-body file://vpc.yaml
